cf1db83bebe026691a094a6de4092f6730053389
gserpa <gusserpa02@gmail.com>
2023-04-22 03:16:08 -0500

    base

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
 const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+const { database } = require('./keys');
 
 // initializations
 const app = express();
@@ -15,18 +19,37 @@ app.engine('.hbs', exphbs.engine({
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
-}))
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
+
 // middlewares
+app.use(session({
+    secret: 'morrosesion',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+}));
+app.use(flash());
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 
 // global variables
+app.use((req, res, next) => {
+    app.locals.success = req.flash('success');
+    next();
+});
 
 // routes
 app.use(require('./routes/'));
+app.use(require('./routes/auth'));
+app.use('/links', require('./routes/links'));
+
 
 // public
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // starting the server
 app.listen(app.get('port'), () => {
