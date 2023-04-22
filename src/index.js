bf7f775eafec24f2a0d67d18c3e3659064d42d30
gserpa <gusserpa02@gmail.com>
2023-04-22 04:59:28 -0500

    base

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const flash = require('connect-flash');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 const { database } = require('./keys');
+const passport = require('passport');
 
 // initializations
 const app = express();
+require('./lib/passport');
 
 
 // settings
@@ -34,6 +36,8 @@ app.use(flash());
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 
 // global variables
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const passport = require('passport');
+
+
+router.get('/signup', (req, res) => {
+    res.render('auth/signup')
+});
+
+router.post('/signup', passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+})  
+    //console.log(req.body)
+);
+
+router.get('/profile', (req, res) => {
+    res.send('profile')
+});
+
+
+
+
 module.exports = router;
